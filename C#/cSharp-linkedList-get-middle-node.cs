383c1e94b3355596b0219c4c635c9e5105512b8f
Kimiko Muwanguzi <kimikomu@gmail.com>
2024-09-20 16:10:36 -0400

    delete space

--- C#/cSharp-linkedList-get-middle-node.cs
+++ C#/cSharp-linkedList-get-middle-node.cs
@@ -1,7 +1,6 @@
 // Given the head of a singly linked list, return the middle node of the linked list.
 // If there are two middle nodes, return the second middle node.
 
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
